95bc0d873cb4e5ea5a2103d71d647c7bebd2a60d
prajapatideepika29 <prajapati.deepika29@gmail.com>
2026-02-12 11:28:42 +0530

    Initial commit

---
+++ src/main/java/org/yourcompany/yourproject/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author deepika
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

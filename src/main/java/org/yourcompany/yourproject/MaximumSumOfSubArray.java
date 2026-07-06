aa4a2911a364ee23e638f8dd33d0a4a2e7740e2e
prajapatideepika29 <prajapati.deepika29@gmail.com>
2026-07-06 22:44:48 +0530

    Kadane's Algorithm

--- src/main/java/org/yourcompany/yourproject/MaximumSumOfSubArray.java
+++ src/main/java/org/yourcompany/yourproject/MaximumSumOfSubArray.java
@@ -1,15 +1,24 @@
 package org.yourcompany.yourproject;
-
+//Kadane's Algorithm
 public class MaximumSumOfSubArray {
     public static int maxSubArray(int[] arr){
      
-        int maxSoFar = arr[0];
-        int currentMax = arr[0];
+        int maxSoFar = arr[0]; //Stores the largest sum found anywhere in the array.
+        int currentMax = arr[0]; // Stores the maximum sum ending at the current index.
 
         for(int i=1; i< arr.length; i++){
 
+            /*For every element, there are two choices:
+            Choice 1
+            Start a new subarray from the current element.
+            arr[i]
+            Choice 2
+            Extend the previous subarray.
+            currentMax + arr[i]
+            Take whichever is larger. */
             currentMax = Math.max(arr[i], currentMax + arr[i]);
-
+            
+            //If the current subarray is better than the best seen so far, update maxSoFar.
             maxSoFar = Math.max(maxSoFar, currentMax);
 
         }

a8befbff83019ba9b8b7b7ab1f15bb0b7912076a
prajapatideepika29 <prajapati.deepika29@gmail.com>
2026-07-06 22:44:22 +0530

    Kadane's Algorithm

---
+++ src/main/java/org/yourcompany/yourproject/MaximumSumOfSubArray.java
@@ -0,0 +1,24 @@
+package org.yourcompany.yourproject;
+
+public class MaximumSumOfSubArray {
+    public static int maxSubArray(int[] arr){
+     
+        int maxSoFar = arr[0];
+        int currentMax = arr[0];
+
+        for(int i=1; i< arr.length; i++){
+
+            currentMax = Math.max(arr[i], currentMax + arr[i]);
+
+            maxSoFar = Math.max(maxSoFar, currentMax);
+
+        }
+        return  maxSoFar;
+
+    }
+    public static void main(String[] args){
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(arr));
+    }
+}
+
